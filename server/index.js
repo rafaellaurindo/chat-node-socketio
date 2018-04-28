b15f09f05f67696f90eb07e55dea53a572f3e65b
Rafael Filho <filho.fael@gmail.com>
2018-04-27 23:21:08 -0300

    Server is running

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    
+    socket.on('username-definido', (username) => { 
+        socket.username = username;
+        io.emit('usuarios-atualizados', { usuario: username, evento: 'entrou'});
+    });
+    
+    socket.on('nova-mensagem', (mensagem) => {
+        io.emit('mensagem', { texto: mensagem.texto, de: socket.username, data: new Date() })
+    });
+
+    socket.on('disconectado', () => {
+        io.emit('usuarios-atualizados', { usuario: socket.username, evento: 'saiu'});
+    });
+    
+});
+
+const porta = process.env.PORT || 3000;
+
+http.listen(porta, () => {
+    console.log('Servidor está rodando em http://localhost:' + porta);
+});
